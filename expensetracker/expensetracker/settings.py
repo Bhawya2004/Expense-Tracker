b95ec204880c8b85a83819ad5cf2cefe40f0a7c7
bhawya2004 <bhawyagulati33@gmail.com>
2026-07-20 20:33:18 +0530

    fix the setting.py for render deployment

--- expensetracker/expensetracker/settings.py
+++ expensetracker/expensetracker/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ── Security ──
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost,bhawya2004.me,expense-tracker.bhawya2004.me', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost,bhawya2004.me,expense-tracker.bhawya2004.me,.onrender.com', cast=Csv())
 
 # Allow OAuth2 over HTTP only in development
 if DEBUG:
